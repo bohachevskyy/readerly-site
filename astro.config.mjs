cf74a7b9e8ce97b65dda401ee91769b4bb050e10
Dmytro Bohachevskyi <dmytro.bohachevskyy@gmail.com>
2026-01-31 18:09:35 -0800

    Astro resources fixes for website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://readerly.ai",
+  base: "/readerly-site/",
   output: "static",
   integrations: [tailwind(), sitemap()],
 });
