d0febe50a46fc8a39d86558aec2160efdcb0ae43
Dmytro Bohachevskyi <dmytro.bohachevskyy@gmail.com>
2026-02-12 20:43:25 -0800

    Sitemap URLs all consistently end with /

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://readerly.ai",
   output: "static",
+  trailingSlash: "always",
   integrations: [tailwind(), sitemap()],
 });
